3b15fe33390b5d8f0d3dd2c07f4a1499974ffbba
Madhu Korada <manoj.korada0000@gmail.com>
2023-03-29 14:47:18 -0400

    few mods for parameterization

--- src/video_subscriber.py
+++ src/video_subscriber.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-
 import rospy
 import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-
 from segmentation_mmseg import *
 
-pub = rospy.Publisher('video_frames_seg', Image, queue_size=10)
+# Access the rosparams
+param_subcriber_topic = rospy.get_param('subcriber_topic')
+param_publisher_topic = rospy.get_param('publisher_topic')
+
+pub = rospy.Publisher(param_publisher_topic, Image, queue_size=10)
 
 def image_callback(data):
     bridge = CvBridge()
@@ -17,16 +19,15 @@ def image_callback(data):
     pub.publish(bridge.cv2_to_imgmsg(seg_frame, encoding="rgb8"))
     
 
-def grayscale_subscriber():
-    rospy.init_node('grayscale_subscriber', anonymous=True)
-    # rospy.Subscriber('video_frames', Image, image_callback)
-    rospy.Subscriber('/device_0/sensor_1/Color_0/image/data', Image, image_callback)
+def image_subscriber():
+    rospy.init_node('image_subscriber', anonymous=True)
+    rospy.Subscriber(param_subcriber_topic, Image, image_callback)
 
     rospy.spin()
 
 if __name__ == '__main__':
     try:
-        grayscale_subscriber()
+        image_subscriber()
     except rospy.ROSInterruptException:
         pass
 
